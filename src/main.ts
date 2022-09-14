484e15946f0932d9892c009de7a0849770f76abb
Nikola Drašković <draskovic26z@gmail.com>
2022-09-14 11:02:41 +0200

    Task Management (REST API), Validation and ErrHand

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // whenever run into validation decorator run the validation pipe
+  await app.listen(3000); //makes server live
+}
+bootstrap();
